28a2e4762a15ee111fbdb0613bf116215dbb51f5
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2015-03-19 01:26:34 +0200

    Improved load times (pushed map and eventbrite to after page load, removed placeholder speakers, now loading speakers images locally)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
 });
 
 $(window).load(function() {
+  $('iframe[data-src]').each(function(index, element) {
+    var $element = $(element);
+    $element.attr('src', $element.attr('data-src'));
+  });
+
   $('.preloader').fadeOut(function() {
     $('body').addClass('loaded');
   });
