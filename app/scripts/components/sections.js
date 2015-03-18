eec8eb92da866b46c5e8567c666e01f0f5eba43e
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2015-03-18 23:00:03 +0200

    Some design fixes

--- app/scripts/components/sections.js
+++ app/scripts/components/sections.js
@@ -24,34 +24,20 @@ $(document).ready(function () {
 
     scenes.push(sectionScene);
 
-    // TODO:  Remove if not re-used before launch
-    //var sectionLetterElement = $(element).find('.section-letter').get(0);
-    //var sectionIllustrationElement = $(element).find('.section-illustration').get(0);
-    //var sectionLetterTween = new TimelineMax()
-    //  .add(TweenMax.to(sectionLetterElement, 1.2, {
-    //    css: {
-    //      top: '-=200'
-    //    },
-    //    ease: Power1.easeInOut
-    //  }));
-    //
-    //var sectionLetterScene = new ScrollMagic.Scene({triggerElement: element, duration: windowHeight})
-    //  .setTween(sectionLetterTween);
-    //
-    //scenes.push(sectionLetterScene);
-    //
-    //var sectionIllustrationTween = new TimelineMax()
-    //  .add(TweenMax.to(sectionIllustrationElement, 1.2, {
-    //    css: {
-    //      top: '-=300'
-    //    },
-    //    ease: Power1.easeInOut
-    //  }));
-    //
-    //var sectionIllustrationScene = new ScrollMagic.Scene({triggerElement: element, duration: windowHeight})
-    //  .setTween(sectionIllustrationTween);
-    //
-    //scenes.push(sectionIllustrationScene);
+    var sectionIllustrationElement = $(element).find('.section-illustration').get(0);
+
+    var sectionIllustrationTween = new TimelineMax()
+      .add(TweenMax.to(sectionIllustrationElement, 1.2, {
+        css: {
+          top: '-=200'
+        },
+        ease: Power1.easeInOut
+      }));
+
+    var sectionIllustrationScene = new ScrollMagic.Scene({triggerElement: element, duration: windowHeight})
+      .setTween(sectionIllustrationTween);
+
+    scenes.push(sectionIllustrationScene);
   });
 
   controller.addScene(scenes);
