4ff8e2768e022daba2b5008c57f53de2c3279914
Igal Steklov <igalst@wix.com>
2015-03-19 15:23:30 +0200

    Mobile navigation

---
+++ app/scripts/components/mobileNavigation.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(document).ready(function () {
+	var dom = {
+		body: $('body'),
+		handler: $('#navToggle'),
+		links: $('.nav-link')
+	};
+
+	var toggle = function(showOrHide) {
+		dom.body.toggleClass('nav-active', showOrHide);
+	};
+
+	dom.handler.click(toggle);
+
+	dom.links.click(function () {
+		toggle(false);
+	});
+});
