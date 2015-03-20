662fd8d07511aa18beb8472939160fc74bc22f4b
Sergey Bolshchikov <sergey@bolshchikov.net>
2015-03-20 17:36:56 +0200

    chore(map): fixing styling

--- app/scripts/components/map.js
+++ app/scripts/components/map.js
@@ -1,63 +1,63 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    function init() {
+  function init() {
 
-        var location = {
-            lat: 32.0767793,
-            long: 34.7852898
-        };
+    var location = {
+      lat: 32.0767793,
+      long: 34.7852898
+    };
 
-        var mapOptions = {
-            zoom: 14,
-            scrollwheel: false,
-            center: new google.maps.LatLng(location.lat, location.long),
-            styles: [{
-                featureType: 'landscape',
-                stylers: [{saturation: -100}, {lightness: 65}, {visibility: 'on'}]
-            }, {
-                featureType: 'poi',
-                stylers: [{saturation: -100}, {lightness: 51}, {visibility: 'simplified'}]
-            }, {
-                featureType: 'road.highway',
-                stylers: [{saturation: -100}, {visibility: 'simplified'}]
-            }, {
-                featureType: 'road.arterial',
-                stylers: [{saturation: -100}, {lightness: 30}, {visibility: 'on'}]
-            }, {
-                featureType: 'road.local',
-                stylers: [{saturation: -100}, {lightness: 40}, {visibility: 'on'}]
-            }, {
-                featureType: 'transit',
-                stylers: [{saturation: -100}, {visibility: 'simplified'}]
-            }, {
-                featureType: 'administrative.province',
-                stylers: [{visibility: 'off'}]
-            }, {
-                featureType: 'water',
-                elementType: 'labels',
-                stylers: [{visibility: 'on'}, {lightness: -25}, {saturation: -100}]
-            }, {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{hue: '#ffff00'}, {lightness: -25}, {saturation: -97}]
-            }]
-        };
+    var mapOptions = {
+      zoom: 14,
+      scrollwheel: false,
+      center: new google.maps.LatLng(location.lat, location.long),
+      styles: [{
+        featureType: 'landscape',
+        stylers: [{saturation: -100}, {lightness: 65}, {visibility: 'on'}]
+      }, {
+        featureType: 'poi',
+        stylers: [{saturation: -100}, {lightness: 51}, {visibility: 'simplified'}]
+      }, {
+        featureType: 'road.highway',
+        stylers: [{saturation: -100}, {visibility: 'simplified'}]
+      }, {
+        featureType: 'road.arterial',
+        stylers: [{saturation: -100}, {lightness: 30}, {visibility: 'on'}]
+      }, {
+        featureType: 'road.local',
+        stylers: [{saturation: -100}, {lightness: 40}, {visibility: 'on'}]
+      }, {
+        featureType: 'transit',
+        stylers: [{saturation: -100}, {visibility: 'simplified'}]
+      }, {
+        featureType: 'administrative.province',
+        stylers: [{visibility: 'off'}]
+      }, {
+        featureType: 'water',
+        elementType: 'labels',
+        stylers: [{visibility: 'on'}, {lightness: -25}, {saturation: -100}]
+      }, {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{hue: '#ffff00'}, {lightness: -25}, {saturation: -97}]
+      }]
+    };
 
-        var mapElement = document.querySelector('.venue-map');
+    var mapElement = document.querySelector('.venue-map');
 
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(location.lat, location.long),
-            map: map,
-            icon: {
-                url: 'images/location-pin.png',
-                scaledSize: new google.maps.Size(28.5, 45.5)
-            },
-            title: ''
-        });
-    }
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(location.lat, location.long),
+      map: map,
+      icon: {
+        url: 'images/location-pin.png',
+        scaledSize: new google.maps.Size(28.5, 45.5)
+      },
+      title: ''
+    });
+  }
 
-    google.maps.event.addDomListener(window, 'load', init);
+  google.maps.event.addDomListener(window, 'load', init);
 });
