a4fe1d6f8ba08035b3e47532d4615960071bdea6
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2015-03-21 23:28:03 +0200

    Logo letters formation is now defined by an organized matrix

--- app/scripts/components/logo.js
+++ app/scripts/components/logo.js
@@ -1,33 +1,67 @@
 'use strict';
 
-var generatePath = function (element) {
-  var points = [],
-      offset = $(element).offset(),
-      elementHeight = $(element).height(),
-      windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-      endPoint = {x: offset.left, y: offset.top},
-    random = function (min, max) {
-      return Math.random() * (max - min) + min;
-    };
+var logoMatrix = [
+  [0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0]
+];
 
-  for (var i = 0; i < 1; i++) {
-    points.push({
-      x: 0,
-      y: random(-endPoint.y, windowHeight - endPoint.y - elementHeight)
-    });
+function randomizeCellInMatrix(matrix, numOfPossibilities) {
+  var random = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  var cellIndex = random(1, numOfPossibilities);
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      var bit = matrix[i][j];
+      if (bit) {
+        cellIndex--;
+
+        if (!cellIndex) {
+          matrix[i][j] = 0;
+          return {x: j, y: i};
+        }
+      }
+    }
   }
+}
+
+function getPixelCordsByMatrixCoords(coords, matrix, element, $container) {
+  var $element = $(element),
+      elementOffset = $element.offset(),
+      elementHeight = $element.height(),
+      containerOffset = $container.offset(),
+      containerWidth = $container.width(),
+      containerOuterWidth = $container.outerWidth(),
+      containerHorizontalPadding = (containerOuterWidth - containerWidth) / 2,
+      windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+      matrixSize = matrix.length,
+      colWidth = containerWidth / matrixSize,
+      rowHeight = windowHeight / matrixSize;
+
+  // Pixel pos calculated as follows:
+  // 1. Reset element pos to (0,0) by subtracting the negative value of its current top/left measure
+  // 2. Move the element to its pixel coord by converting (x,y) matrix coords to (left, top) pixel measures
+  // 3. Move the element to the container left offset by adding the container's x offset and its horizontal padding
+  // 4. Move the element to the vertical center of its row in the by adding half of the row height
+  //    and subtracting half of the element height
 
   return {
-    curviness: 1.25,
-    autoRotate: false,
-    values: points
+    x: (-elementOffset.left) + (coords.x * colWidth) + containerOffset.left + containerHorizontalPadding,
+    y: (-elementOffset.top) + (coords.y * rowHeight) + (rowHeight / 2) - (elementHeight / 2)
   };
-};
+}
 
 $(document).ready(function () {
   var logo_letters = $('.intro .letter'),
     scenes = [],
-    controller = new ScrollMagic.Controller();
+    controller = new ScrollMagic.Controller(),
+    $container = $('h1.logo');
 
   var pinIntroScene = new ScrollMagic.Scene({duration: 900, offset: 0})
     .setPin('.intro', {pushFollowers: false});
@@ -35,10 +69,17 @@ $(document).ready(function () {
   scenes.push(pinIntroScene);
 
   for (var i = 0; i < logo_letters.length; i++) {
+    var pos = randomizeCellInMatrix(logoMatrix, logo_letters.length - i);
+    var pxs = getPixelCordsByMatrixCoords(pos, logoMatrix, logo_letters[i], $container);
+
     var letterTween = new TimelineMax()
       .add(TweenMax.from(logo_letters[i], 1.2, {
         css: {
-          bezier: generatePath(logo_letters[i])
+          bezier: {
+            curviness: 1.25,
+            autoRotate: false,
+            values: [pxs]
+          }
         },
         ease: Power1.easeInOut
       }));
